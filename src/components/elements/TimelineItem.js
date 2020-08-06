21e563ffaa2b181e27436b51a35c34bd211a8c73
LAPLANCHE Maxime <laplanche.maxime@outlook.fr>
2020-08-06 20:11:54 +0200

    update timeline item disposition

--- src/components/elements/TimelineItem.js
+++ src/components/elements/TimelineItem.js
@@ -7,8 +7,16 @@ function TimelineItem(props) {
         <img src="" alt="" />
       </div>
       <div className="timeline-content">
-        <p className="heading">{props.date}</p>
+          {console.log(props.endDate)}
+          {props.endDate === 'Invalid Date' &&
+            <p className="heading">{props.date} - now </p>
+          }
+          {props.endDate !== 'Invalid Date' &&
+          <p className="heading">{props.date} - {props.endDate}  </p>
+          }
+
         <h1 className="title is-4">{props.company}</h1>
+        <p style={{marginTop: "-25px", marginBottom: "20px"}}>{props.position}</p>
         <p style={{ maxWidth: "25em" }}>{props.summary}</p>
       </div>
     </div>
